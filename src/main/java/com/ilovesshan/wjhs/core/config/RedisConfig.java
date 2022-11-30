48bb468356b48a06df2a2fd1cddb94c1ab993b78
wangyifeng <2665939276@qq.com>
2022-11-30 13:40:10 +0800

    feat: redis常用方法封装以及配置redis序列化

---
+++ src/main/java/com/ilovesshan/wjhs/core/config/RedisConfig.java
@@ -0,0 +1,28 @@
+package com.ilovesshan.wjhs.core.config;
+
+import com.alibaba.fastjson.support.spring.GenericFastJsonRedisSerializer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.RedisSerializer;
+
+@Configuration
+public class RedisConfig {
+
+    @Bean(name = "redisTemplate")
+    public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory factory) {
+        RedisTemplate<String, Object> template = new RedisTemplate<>();
+        RedisSerializer<Object> redisSerializer = new GenericFastJsonRedisSerializer();
+        template.setConnectionFactory(factory);
+        //key序列化方式
+        template.setKeySerializer(redisSerializer);
+        //value序列化
+        template.setValueSerializer(redisSerializer);
+        //value hashmap序列化
+        template.setHashValueSerializer(redisSerializer);
+        //key hashmap序列化
+        template.setHashKeySerializer(redisSerializer);
+        return template;
+    }
+}
