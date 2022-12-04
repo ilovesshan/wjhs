b8a607f281e0459cf82debbe3b15493f5289d990
wangyifeng <2665939276@qq.com>
2022-12-04 14:17:14 +0800

    chore: 附件模块 swagger文档

--- src/main/java/com/ilovesshan/wjhs/controller/AttachmentController.java
+++ src/main/java/com/ilovesshan/wjhs/controller/AttachmentController.java
@@ -4,6 +4,8 @@ import com.ilovesshan.wjhs.beans.converter.AttachmentConverter;
 import com.ilovesshan.wjhs.beans.pojo.Attachment;
 import com.ilovesshan.wjhs.service.AttachmentService;
 import com.ilovesshan.wjhs.utils.R;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
@@ -16,6 +18,7 @@ import org.springframework.web.multipart.MultipartFile;
  * @description:
  */
 
+@Api(tags = "附件模块")
 @RestController
 @RequestMapping("attachments")
 public class AttachmentController {
@@ -26,6 +29,7 @@ public class AttachmentController {
     @Autowired
     private AttachmentConverter attachmentConverter;
 
+    @ApiOperation("上传附件")
     @PostMapping
     public R upload(@RequestParam("file") MultipartFile file) {
         String attachmentId = attachmentService.upload(file);
@@ -33,12 +37,16 @@ public class AttachmentController {
         return R.success(R.SUCCESS_ATTACHMENT_UPLOAD, attachmentConverter.po2vo(attachment));
     }
 
+
+    @ApiOperation("根据ID查询附件")
     @GetMapping("/{id}")
     public R select(@PathVariable String id) {
         Attachment attachment = attachmentService.selectById(id);
         return R.success(R.SUCCESS_MESSAGE_SELECT, attachmentConverter.po2vo(attachment));
     }
 
+
+    @ApiOperation("删除附件")
     @DeleteMapping("/{id}")
     public R deleteById(@PathVariable String id) {
         attachmentService.deleteById(id);
