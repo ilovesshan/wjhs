6b4c894feb1f6d9c6a3b8d876787ceae6ee430c5
ilovesshan <2665939276@qq.com>
2022-12-15 14:26:20 +0800

    docs: swagger Api名称

--- src/main/java/com/ilovesshan/wjhs/controller/NoticeController.java
+++ src/main/java/com/ilovesshan/wjhs/controller/NoticeController.java
@@ -70,7 +70,7 @@ public class NoticeController {
 
 
     @DeleteMapping("/{id}")
-    @ApiOperation("根据ID查询公告")
+    @ApiOperation("根据ID删除")
     public R deleteById(@PathVariable String id) {
         boolean isSuccess = noticeService.deleteById(id);
         return isSuccess ? R.success(R.SUCCESS_MESSAGE_DELETE) : R.fail(R.ERROR_MESSAGE_DELETE);
