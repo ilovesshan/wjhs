b746d460b5c37b0516522e1c31fbb36be50c736a
ilovesshan <2665939276@qq.com>
2022-12-27 20:58:50 +0800

    refactor: text update

--- src/main/java/com/ilovesshan/wjhs/beans/dto/AccountRecordCreateDto.java
+++ src/main/java/com/ilovesshan/wjhs/beans/dto/AccountRecordCreateDto.java
@@ -20,7 +20,7 @@ public class AccountRecordCreateDto {
 
 
     @NotNull(message = "交易金额不能为空")
-    @DecimalMin(value = "0", inclusive = false, message = "用户价格必须大于0")
+    @DecimalMin(value = "0", inclusive = false, message = "交易金额必须大于0")
     @Digits(integer = 10, fraction = 2, message = "用户价格 最大长度:10，允许精度:2")
     private double tradingMoney;
 
